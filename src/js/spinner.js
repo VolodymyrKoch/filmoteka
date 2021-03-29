5cc012788056cd15be9940308929181d9400e10d
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2021-03-29 21:44:18 +0300

    devstart

--- src/js/spinner.js
+++
@@ -1,8 +0,0 @@
-import '../css/spinner.css';
-export function spinnerOn() {
-    document.querySelector('#spinner').classList.remove('is-hidden');
-  }
-
-export function spinnerOff() {
-    document.querySelector('#spinner').classList.add('is-hidden');
-  }
